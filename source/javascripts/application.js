8921afe9de89e59dc55aac6177eb48405158018f
Eric Rouach <erouach@hotmail.fr>
2017-09-13 21:02:41 +0200

    popover contact

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -5,15 +5,6 @@
 $(document).ready(function(event){
   $(function () {
     $('[data-toggle="popover"]').popover()
-    html : true,
-    content: function() {
-      return $('#popover_content_wrapper').html();
-    }
   });
 });
-
-
-
-
-
 
