13a800f81475e6adfdfe16ed5c15114cba56e124
Eric Rouach <erouach@hotmail.fr>
2017-09-13 20:07:33 +0200

    popover

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,19 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function(event){
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+    html : true,
+    content: function() {
+      return $('#popover_content_wrapper').html();
+    }
+  });
+});
+
+
+
+
+
+
